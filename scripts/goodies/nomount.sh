f64d1114bc8b1d2543d806d916bbb3a865ffd67f
Dr.HSD <socialdoctor555@gmail.com>
2026-07-26 17:05:56 +0530

    Simplify nomount.sh by removing selector case
    Removed case handling for NOMOUNT_SELECTOR and related messages.
    
    Signed-off-by: Dr.HSD <socialdoctor555@gmail.com>

--- scripts/goodies/nomount.sh
+++ scripts/goodies/nomount.sh
@@ -5,8 +5,6 @@ export NOMOUNT_PATCH="https://github.com/maxsteeel/nomount/raw/refs/heads/master
 export NOMOUNT_CODE="https://github.com/maxsteeel/nomount/raw/refs/heads/master/kernel/src/nomount.c"
 export NOMOUNT_HEADER="https://github.com/maxsteeel/nomount/raw/refs/heads/master/kernel/src/nomount.h"
 
-case "$NOMOUNT_SELECTOR" in
-    nomount)
         # Start of nomount integration
         echo "-- Setting up nomount..."
 
@@ -17,12 +15,4 @@ case "$NOMOUNT_SELECTOR" in
         
         # Enable the necessary Nomount configs
         echo "CONFIG_NOMOUNT=y" >> $MAIN_DEFCONFIG
-        ;;
-    none|"")
-        echo "-- Nomount is not selected."
-        ;;
-    *)
-        echo "- Invalid NOMOUNT_SELECTOR: $NOMOUNT_SELECTOR. Valid options: nomount, none."
-        exit 1
-        ;;
-esac
+        
